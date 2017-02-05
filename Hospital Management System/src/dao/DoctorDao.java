8caeae5af5db1deb5849e3200a87a4ecdf76f056
TUSHAR <TUSHAR@Tushar>
2017-02-05 16:44:13 +0530

    Completed Doctor Dao Implementation with successful JUnit testing.

--- Hospital Management System/src/dao/DoctorDao.java
+++ Hospital Management System/src/dao/DoctorDao.java
@@ -1,13 +1,14 @@
 package dao;
 
+import java.sql.SQLException;
 import java.util.TreeSet;
 
 import bean.Doctor;
 
 public interface DoctorDao {
-	public void insertDoctor(Doctor newDoctor);
-	public Doctor deleteDoctor(int doctorId);
-	public void updateDoctor(int doctorId, Doctor renewDoctor);
-	public Doctor displayDoctor(int doctorId);
-	public TreeSet<Doctor> displayAllDoctors();
+	public void insertDoctor(Doctor newDoctor) throws ClassNotFoundException, SQLException;
+	public boolean deleteDoctor(int doctorId) throws ClassNotFoundException, SQLException;
+	public void updateDoctor(int doctorId, Doctor renewDoctor) throws ClassNotFoundException, SQLException;
+	public Doctor displayDoctor(int doctorId) throws ClassNotFoundException, SQLException;
+	public TreeSet<Doctor> displayAllDoctors() throws SQLException, ClassNotFoundException;
 }
