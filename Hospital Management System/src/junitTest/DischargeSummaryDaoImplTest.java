9ab4a8a3128e669c94623477257dd56a3702ed3b
Deepika Chhabra <dc4n999@gmail.com>
2017-02-07 10:45:45 +0530

    Adding DischargeSummaryDaoImpl and DischargeSummaryDaoImplTest

---
+++ Hospital Management System/src/junitTest/DischargeSummaryDaoImplTest.java
@@ -0,0 +1,44 @@
+package junitTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DischargeSummaryDaoImplTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testInsertDischargeSummary() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteDischargeSummary() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateDischargeSummary() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayDischargeSummary() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayAllDischargeSummarys() {
+		fail("Not yet implemented");
+	}
+
+}
