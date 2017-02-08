63204a8c681d13e57fe336c5161bfb80307a2f19
Debanjan <debanjan0420@gmail.com>
2017-02-08 12:55:12 +0530

    Completed Negative Testing

--- Hospital Management System/src/junitTest/MedicineDaoImplTest.java
+++ Hospital Management System/src/junitTest/MedicineDaoImplTest.java
@@ -40,6 +40,20 @@ public class MedicineDaoImplTest {
 		assertTrue(md.insertMedicine(medicine));
 	}
 
+	@Test(expected=SQLException.class)
+	public void testInsertMedicineNegative() throws ClassNotFoundException, SQLException {	
+		Medicine medicine = new Medicine();
+		medicine.setsNo(1);
+		medicine.setMedicineName("abc");
+		medicine.setQuantity(2);
+		medicine.setDosage("def");
+		medicine.setPrice(123);
+		medicine.setPatientId(123);
+		medicine.setBillNo(12345);
+		
+		assertFalse(md.insertMedicine(medicine));
+	}
+	
 	@Test
 	public void testDeleteMedicine() throws ClassNotFoundException, SQLException {
 		md.deleteMedicine(1);
@@ -53,6 +67,11 @@ public class MedicineDaoImplTest {
 		medicine.setBillNo(12345);
 		md.insertMedicine(medicine);
 		assertTrue(md.deleteMedicine(1));
+	}
+	
+	@Test
+	public void testDeleteMedicineNegative() throws ClassNotFoundException, SQLException {
+		assertFalse(md.deleteMedicine(12131));
 	}
 
 	@Test
@@ -78,6 +97,21 @@ public class MedicineDaoImplTest {
 		assertTrue(md.updateMedicine(1,newmedicine));
 
 	}
+	
+	@Test
+	public void testUpdateMedicineNegative() throws ClassNotFoundException, SQLException
+	{
+		Medicine newmedicine = new Medicine();
+		newmedicine.setsNo(1);
+		newmedicine.setMedicineName("abc");
+		newmedicine.setQuantity(2);
+		newmedicine.setDosage("def");
+		newmedicine.setPrice(123);
+		newmedicine.setPatientId(123);
+		newmedicine.setBillNo(12345);
+		
+		assertFalse(md.updateMedicine(2343, newmedicine));
+	}
 
 	@Test
 	public void testDisplayMedicine() throws ClassNotFoundException, SQLException {
@@ -95,6 +129,10 @@ public class MedicineDaoImplTest {
 		assertEquals(medicine, md.displayMedicine(1));
 	}
 
+	@Test
+	public void testDisplayMedicineNegative() throws ClassNotFoundException, SQLException {
+		assertEquals(0,md.displayMedicine(234234).getsNo());
+	}
 	@Test
 	public void testDisplayAllMedicines() throws ClassNotFoundException, SQLException {
 		assertNotEquals(null, md.displayAllMedicines());
