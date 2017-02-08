0261fe5f3c79416645176a232ce7c39dddbe2e4e
Deepika Chhabra <dc4n999@gmail.com>
2017-02-08 11:35:11 +0530

    Completed Negative Testing for PersonDaoImpl

--- Hospital Management System/src/dao/PersonDao.java
+++ Hospital Management System/src/dao/PersonDao.java
@@ -7,8 +7,8 @@ import bean.Person;
 
 public interface PersonDao {
 	public boolean insertPerson(Person newPerson) throws SQLException, ClassNotFoundException;
-	public boolean deletePerson(int personId) throws ClassNotFoundException, SQLException;
-	public boolean updatePerson(int personId, Person renewPerson) throws ClassNotFoundException, SQLException;
-	public Person displayPerson(int personId) throws ClassNotFoundException, SQLException;
+	public boolean deletePerson(String personId) throws ClassNotFoundException, SQLException;
+	public boolean updatePerson(String personId, Person renewPerson) throws ClassNotFoundException, SQLException;
+	public Person displayPerson(String personId) throws ClassNotFoundException, SQLException;
 	public TreeSet<Person> displayAllPersons() throws ClassNotFoundException, SQLException;
 }
